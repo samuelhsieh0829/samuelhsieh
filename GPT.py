d19798e9ba6bd1ba418aa6f29f17fa9f984a9418
samuelhsieh0829 <66865408+samuelhsieh0829@users.noreply.github.com>
2024-06-20 10:28:49 +0800

    first commit

---
+++ GPT.py
@@ -0,0 +1,48 @@
+import openai
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+class GPT:
+    def __init__(self) -> None:
+        self._gpt = openai.OpenAI(api_key=os.getenv("GPTTOKEN"))
+        self.base_prompt_path = "base_prompt.txt"
+        with open(self.base_prompt_path, "r") as f:
+            self.base_prompt = f.read()
+        self.chat_history = []
+        self.model = os.getenv("GPTMODEL")
+        self.history_limit = int(os.getenv("LIMIT"))
+    
+    def load_prompt(self) -> None:
+        with open(self.base_prompt_path, "r") as f:
+            self.base_prompt = f.read()
+        print("Prompt loaded")
+    
+    def message_request(self, message:str, user:str) -> str:
+        if len(self.chat_history) > self.history_limit:
+            self.chat_history = []
+            print("History cleared")
+        if len(self.chat_history) == 0:
+            self.chat_history.append({"role": "user", "content": "{}\n{} : {}".format(self.base_prompt, user, message)})
+        else:
+            self.chat_history.append({"role": "user", "content": message})
+        response = self._send_message(self.chat_history)
+        content = response.choices[0].message.content
+        self.chat_history.append({"role": "assistant", "content": content})
+        print(response.usage)
+        return content
+    
+    def _send_message(self, message:str):
+        completion = self._gpt.chat.completions.create(
+        messages=message,
+        model=self.model,
+        max_tokens=1024
+        )
+        return completion
+    
+    def clear_history(self):
+        self.chat_history = []
+
+    def change_base_prompt(self, path):
+        self.base_prompt_path = path
---
+++ main.py
@@ -0,0 +1,92 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import dotenv
+import os
+import GPT
+import time
+
+dotenv.load_dotenv()
+
+dctoken = os.getenv("DCTOKEN")
+owner_id = int(os.getenv("OWNERID"))
+cooldown = int(os.getenv("COOLDOWN"))
+
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+gpt = GPT.GPT()
+
+used_time = time.time()
+
+def check_user(user_id:int):
+    if user_id == owner_id:
+        return True
+    else:
+        return False
+
+@client.event
+async def on_ready():
+    print("Logging as {}".format(client.user))
+    try:
+        await client.tree.sync()
+        game = discord.Game("Cosplay 謝恩")
+        await client.change_presence(activity=game)
+    except Exception as e:
+        print(e)
+
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+    global used_time
+    if time.time() - used_time < cooldown:
+        print(time.time() - used_time)
+        return
+    if not (("<@551395982756282369>" in message.content) or ("<@1230789209082564619>" in message.content)):
+        return
+    async with message.channel.typing():
+        global gpt
+        print(message.content)
+        response = gpt.message_request(message.content, user=message.author.name)
+        used_time = time.time()
+        await message.channel.send(response)
+    
+@client.tree.command(name="load_prompt", description="Reload base Prompt")
+async def load_prompt(ctx: discord.Interaction):
+    if check_user:
+        global gpt
+        gpt.load_prompt()
+        print("Prompt reloaded")
+        await ctx.response.send_message("Reloaded prompt")
+    else:
+        print(f"{ctx.user.name} tried to reload prompt")
+        await ctx.response.send_message(f"<@{ctx.user.id}>是傻逼")
+
+@client.tree.command(name="history", description="View chat history")
+async def load_prompt(ctx: discord.Interaction):
+    if check_user:
+        print(gpt.chat_history)
+        await ctx.response.send_message(gpt.chat_history)
+    else:
+        print(f"{ctx.user.name} tried to see chat history")
+        await ctx.response.send_message(f"<@{ctx.user.id}>是傻逼")
+
+@client.tree.command(name="clear_history", description="Clear chat history")
+async def load_prompt(ctx: discord.Interaction):
+    if check_user:
+        gpt.clear_history()
+        await ctx.response.send_message("Done")
+    else:
+        print(f"{ctx.user.name} tried to clear chat history")
+        await ctx.response.send_message(f"<@{ctx.user.id}>是傻逼")
+
+@client.tree.command(name="change_prompt", description="Change base prompt")
+async def load_prompt(ctx: discord.Interaction, filename:str):
+    if check_user:
+        gpt.clear_history()
+        gpt.change_base_prompt(filename)
+        await ctx.response.send_message("Done")
+    else:
+        print(f"{ctx.user.name} tried to clear chat history")
+        await ctx.response.send_message(f"<@{ctx.user.id}>是傻逼")
+
+client.run(token=dctoken)
